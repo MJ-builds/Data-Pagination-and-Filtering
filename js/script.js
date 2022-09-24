88b239f51bd706ba6fe7be1e487e349010ddeeba
MJ <code.za@proton.me>
2022-09-24 18:49:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,24 +2,23 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
 document.addEventListener('DOMContentLoaded', () => {
 
-let itemsPerPage = 9; //Global scope
+let itemsPerPage = 9; //scope
 
 //This function will create and insert/append the elements needed to display a "page" of nine students
-function showPage (list,page) {
+function showPage (list , page) {
 
    // create two variables which will represent the index for the first and last student on the page
    let startIndex = ((page * itemsPerPage) - itemsPerPage);
-   let endIndex = ((page * itemsPerPage) -1);
+   let endIndex = ((page * itemsPerPage) - 1 );
 
    // select the element with a class of `student-list` and assign it to a variable
    let studentList = document.querySelector('.student-list');
 
   // set the innerHTML property of the variable you just created to an empty string
       studentList.innerHTML = '';
-      let student ='';
+      let student = '';
 
   // loop over the length of the `list` parameter
   for (let i = 0; i < list.length; i++) {
@@ -42,8 +41,8 @@ function showPage (list,page) {
       `;
    }
   }
-  // insert the above elements
-  studentList.insertAdjacentHTML('beforeend',student); //insert element on to page
+// insert the above elements
+studentList.insertAdjacentHTML('beforeend',student);
 }; //end of showPage function
 
 function addPagination(list) {
@@ -55,15 +54,15 @@ function addPagination(list) {
 
    // set the innerHTML property of the variable you just created to an empty string
    linkList.innerHTML = '';
-   let button=''; 
-   
+   let button= ''; 
+
    // loop over the number of pages needed
    for (let i = 1; i <= numOfPages; i++){
       // create the elements needed to display the pagination button
       button += `<li><button type='button'>${[i]}</button></li>`;
    }
 // insert the above elements
- linkList.insertAdjacentHTML('beforeend',button);
+linkList.insertAdjacentHTML('beforeend',button);
 
    // give the first pagination button a class of "active"
  
