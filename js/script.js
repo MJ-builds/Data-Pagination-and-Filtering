07bc847a34b04bb7fb27bbeeadb1a6a4e4facbf2
MJ <code.za@proton.me>
2022-09-24 13:08:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,10 +15,6 @@ function showPage (list,page) {
    let itemsPerPage = 9;
    let startIndex = ((page * itemsPerPage) - itemsPerPage);
    let endIndex = ((page * itemsPerPage) -1);
-   //console.log(list);
-   //console.log(page);
-
-
 
   // select the element with a class of `student-list` and assign it to a variable
   let studentList = document.querySelector('.student-list');
@@ -42,62 +38,48 @@ insertAdjacentHTML method and beforeend position.'*/
 
       let li = document.createElement('li');
       li.className = 'student-item cf';
+
       let divDetails = document.createElement('div'); 
       divDetails.className = 'student-details';
       li.appendChild(divDetails);
+
       let img = document.createElement('img');
       img.className = 'avatar';
       img.src= `${studentItem.picture.large}`;
       img.alt = 'Profile Picture';
       divDetails.appendChild(img);
-      //li.insertAdjacentElement('afterend',img); if appendChild doesnt work in the correct order
+
       let name = document.createElement('h3');
       name.textContent = `${studentItem.name.first} ${studentItem.name.last}`;
       divDetails.appendChild(name);
+
       studentList.appendChild(li);
+
       let email = document.createElement('span');
       email.className = 'email';
       email.textContent = `${studentItem.email}`;
       divDetails.appendChild(email);
+
       let divJoined = document.createElement('div');
       divJoined.className = 'joined-details';
       li.appendChild(divJoined);
+
       let date = document.createElement('span');
       date.className = 'date';
       date.textContent = `Joined ${studentItem.registered.date}`;
       divJoined.appendChild(date);
    }
-
   }
-
 }; //end of showPage function
 
 showPage(data,1); //Testing showPage functionality.
-
-//For my ref for the above - to delete.
-
-   // EXAMPLE - Student list item:
-
-   // <li class="student-item cf">
-   //   <div class="student-details">
-   //     <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
-   //     <h3>Ethel Dean</h3>
-   //     <span class="email">ethel.dean@example.com</span>
-   //   </div>
-   //   <div class="joined-details">
-   //     <span class="date">Joined 12-15-2005</span>
-   //   </div>
-   // </li>
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
-
-
 // Call functions
-
 
 
 }); // Closing out DOMContentLoaded
