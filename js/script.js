e511a3b6e0f5866057bd15f549f825d919b1781f
MJ <code.za@proton.me>
2022-09-25 15:06:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,29 +4,28 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 document.addEventListener('DOMContentLoaded', () => {
 
-let itemsPerPage = 9; //scope
+const itemsPerPage = 9;
 
-//This function will create and insert/append the elements needed to display a "page" of nine students
+// this function creates the elements needed to display a page of nine students
 function showPage (list , page) {
 
-   // create two variables which will represent the index for the first and last student on the page
+   // variables that represent the index for the first and last student on the page
    let startIndex = ((page * itemsPerPage) - itemsPerPage);
    let endIndex = ((page * itemsPerPage) - 1 );
 
-   // select the element with a class of `student-list` and assign it to a variable
+   // select the element with a class of `student-list`
    let studentList = document.querySelector('.student-list');
 
-  // set the innerHTML property of the variable you just created to an empty string
       studentList.innerHTML = '';
       let student = '';
 
   // loop over the length of the `list` parameter
   for (let i = 0; i < list.length; i++) {
 
+   //selecting those specific students in the array that fall between startIndex and endIndex
    if(i >= startIndex && i <= endIndex) {
       let studentItem = list[i];
-
-      //recreated the code as a simple template literal. Code far easier to read vs prior version.
+      // create the elements needed to display the student data
       student += `
             <li class='student-item cf'>
                <div class='student-details'>
@@ -43,46 +42,56 @@ function showPage (list , page) {
   }
 // insert the above elements
 studentList.insertAdjacentHTML('beforeend',student);
-}; //end of showPage function
+}; // end of showPage function
 
+/* creates the pagination functionality, dynamically adding buttons in a list
+for navigation to display up to 9 students per page */
 function addPagination(list) {
-   // create a variable to calculate the number of pages needed
+   // variable that calculates the number of pages needed
    let numOfPages = Math.ceil(list.length / itemsPerPage);
  
-   // select the element with a class of `link-list` and assign it to a variable
+   // select the element with a class of `link-list`
    let linkList = document.querySelector('.link-list');
 
-   // set the innerHTML property of the variable you just created to an empty string
    linkList.innerHTML = '';
    let button= ''; 
 
    // loop over the number of pages needed
    for (let i = 1; i <= numOfPages; i++){
       // create the elements needed to display the pagination button
-      button += `<li><button type='button'>${[i]}</button></li>`;
+      button += `
+            <li>
+               <button type='button'>${[i]}</button>
+            </li>
+      `;
    }
 // insert the above elements
 linkList.insertAdjacentHTML('beforeend',button);
 
-   // give the first pagination button a class of "active"
- 
-   // create an event listener on the `link-list` element
-     // if the click target is a button:
-       // remove the "active" class from the previous button
-       // add the active class to the clicked button
-       // call the showPage function passing the `list` parameter and page to display as arguments
+// select the first button from the list
+let firstBtnActive = document.querySelector('li button');
+ // give selected button a class name of "active"
+firstBtnActive.className = 'active';
+
+// event listener (click) for buttons 'list'
+linkList.addEventListener('click', (e) => {
+   // conditional that checks if the tagName of event target (element clicked) is a button
+   if (e.target.tagName === 'BUTTON') { 
+
+      let active = document.querySelector('.active');
+
+      active.className='';
+      e.target.className='active';
+
+      // call the showPage function
+      showPage(list,e.target.textContent);
+   }
+});
 
 } // end of addPagination function
 
-addPagination(data);
-showPage(data,1); //Testing showPage functionality.
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
 // Call functions
+showPage(data,1);
+addPagination(data);
 
-
-}); // Closing out DOMContentLoaded
+}); // end of DOMContentLoaded
